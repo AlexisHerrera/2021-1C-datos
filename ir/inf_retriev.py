1cbe46e94c50be067124c97e99862a909b3f1fd1
AlexisHerrera <52416190+AlexisHerrera@users.noreply.github.com>
2021-06-08 17:42:29 -0300

    Indice documentos concatenado y distancias

--- ir/inf_retriev.py
+++ ir/inf_retriev.py
@@ -11,16 +11,8 @@ def crear_lexico_concatenado(terminos):
     """
     Crear lexico recibe una lista de palabras y devuelve una tupla de listas.
     La primera es el índice de punteros a los terminos concatenados y la segunda son los términos concatenados.
-    """ 
-    terminos.sort()
-    indice = [0]
-    len_anterior = 0
-    for termino in terminos:
-        indice.append(len(termino)+len_anterior)
-        len_anterior += len(termino)
-    indice.pop()
-    lexico = indice,terminos
-    return lexico
+    """
+    return crear_indice_concatenado(terminos)
 
 def crear_lexico_front_coding(terminos,n=None):
     if n ==None:
@@ -66,6 +58,28 @@ def crear_lexico(terminos,modo='c',n=None):
         return crear_lexico_front_coding(terminos,n)
 
 def print_lexico(lexico):
+    print_indice(lexico)
+
+def print_lexico_front_coding(lexico):
+    iguales,distintos,indice,front_coding_terminos = lexico
+    print("i|d|p")
+    if not(len(iguales) == len(distintos) and len(distintos) == len(indice)):
+        return
+    for i in range(len(iguales)):
+        print(f"{iguales[i]}|{distintos[i]}|{indice[i]}")
+    print_lexico((indice,front_coding_terminos))
+
+def crear_indice_concatenado(terminos):
+    indice = [0]
+    len_anterior = 0
+    for termino in terminos:
+        indice.append(len(termino)+len_anterior)
+        len_anterior += len(termino)
+    indice.pop()
+    lexico = indice,terminos
+    return lexico
+
+def print_indice(lexico):
     """Dado una tupla de indice y terminos imprime el índice de léxicos de forma legible"""
     indice,terminos = lexico
     for termino in terminos:
@@ -78,12 +92,27 @@ def print_lexico(lexico):
             largo_espacios -= len(str(indice[i]))-1
         print(indice[i],end=" "*largo_espacios)
         posicion += 1
+def crear_indice_distancias(documentos):
+    documentos_en_distancia = []
+    for lista_documento in documentos:
+        distancia = ""
+        num_doc_actual=0
+        for i in range(len(lista_documento)):
+            if i == 0:
+                distancia += lista_documento[0]
+            else:
+                diferencia = int(lista_documento[i])-num_doc_actual
+                distancia += str(diferencia)
+            num_doc_actual += int(lista_documento[i])
+        documentos_en_distancia.append(distancia)
+    return crear_indice_concatenado(documentos_en_distancia)
 
-def print_lexico_front_coding(lexico):
-    iguales,distintos,indice,front_coding_terminos = lexico
-    print("i|d|p")
-    if not(len(iguales) == len(distintos) and len(distintos) == len(indice)):
-        return
-    for i in range(len(iguales)):
-        print(f"{iguales[i]}|{distintos[i]}|{indice[i]}")
-    print_lexico((indice,front_coding_terminos))
+def crear_indice_documentos(documentos,modo='c'):
+    if modo =='c':
+        indice_documentos = crear_indice_concatenado(documentos)
+    elif modo == 'd':
+        indice_documentos = crear_indice_distancias(documentos)
+    return indice_documentos
+
+def print_indice_documentos(indice_documento):
+    print_indice(indice_documento)
